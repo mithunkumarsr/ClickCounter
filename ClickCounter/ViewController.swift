5b9cb3d0435c76d70c0a7c31b4af144a27f7d17c
mithunkumarsr <mithunkumar.sr@gmail.com>
2017-05-02 13:41:01 +0530

    Intitial commit of click counter

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -12,13 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var label = UILabel()
+        label.frame = CGRectMake(150,150,60,60)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
